c00f522b10a68f45bd9c8738c05befa7f0f3f229
Ewerton Madruga <emadruga@gmail.com>
2014-02-18 09:42:43 -0300

    - Output price as float.

--- test-src/price-americanas.py
+++ test-src/price-americanas.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python
 from lxml import etree
 import re
+
+def str2float(str) :
+	response = float(str)
+	return response
+
 
 parser = etree.HTMLParser()
 
@@ -8,19 +14,22 @@ page = "../sample-data/ventilador-americanas.html"
 
 tree = etree.parse(page, parser)
 
-root = tree.getroot();
-print etree.tostring(root, pretty_print=True)
+#root = tree.getroot();
+#print etree.tostring(root, pretty_print=True)
+
+tree.write("out.xml")
 
 myList = tree.xpath("//td[@class='webkit-line-content' and text()='R$']/text()");
 
 print "List: ", myList
 
-count = 0
+prices = []
 for str in myList:
 
 	price = re.findall(r"[-+]?\d*\,\d+|\d+", str)
-
 	if (len(price) > 0):
-		count += 1
-		if (count > 1) :
-			print "Price: ", price[0]
+		prices.append(price[0])
+			
+priceStr = prices[-1].replace(",",".")
+priceFloat = str2float(priceStr)
+print "Price (float): %+10.3f" % priceFloat
